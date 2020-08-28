c24bf241172aa169be22baadf1798313e3d273c3
Nacho Cabanes <nacho@nachocabanes.com>
2020-08-28 12:51:16 +0200

    Godot 2020: Estado tras el video 4 (enemigos)

---
+++ godot2020/proyecto/Marcianos/Enemigo.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+export var pixelesPorSegundo : int = 180
+var velocidadActual = pixelesPorSegundo
+
+func _ready():
+	pass
+
+func _process(delta):
+	position.x += velocidadActual * delta
+	if position.x < 100 or position.x > 900:
+		velocidadActual = - velocidadActual
