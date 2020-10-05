67233efd3462317497d8645e08fe08e4e140f274
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-05 09:21:05 +0200

    Unity 2020 - Estado en el video 004 (mover nave)

---
+++ unity2020/01marcianos/proyecto/Assets/Scripts/Nave.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(horizontal * 0.1f, 0, 0);
+    }
+}
