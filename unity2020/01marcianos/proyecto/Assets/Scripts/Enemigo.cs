8e7ef9ffb4b71c2467dc7695c6ee31ad2889dde3
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-05 09:24:16 +0200

    Unity 2020 - Estado en el video 007 (enemigo móvil)

---
+++ unity2020/01marcianos/proyecto/Assets/Scripts/Enemigo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    private float velocidadX = 2;
+    private float velocidadY = -1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(
+           velocidadX * Time.deltaTime,
+           velocidadY * Time.deltaTime, 
+           0);
+
+        if ((transform.position.x < -4) 
+                || (transform.position.x > 4))
+            velocidadX = -velocidadX;
+        if ((transform.position.y < -2.5)
+                || (transform.position.y > 2.5))
+            velocidadY = -velocidadY;
+    }
+}
