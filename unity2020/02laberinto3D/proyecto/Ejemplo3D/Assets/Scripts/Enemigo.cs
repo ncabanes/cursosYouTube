f2c4cd7ff4ea7cbbda85da9f5e7d921960da8d28
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-07 08:37:49 +0200

    Unity 2020 - Estado en el video 030 (Waypoints)

---
+++ unity2020/02laberinto3D/proyecto/Ejemplo3D/Assets/Scripts/Enemigo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    [SerializeField] Transform[] waypoints;
+    Vector3 siguientePosicion;
+    float velocidad = 2.0f;
+    float distanciaCambio = 0.5f;
+    int numeroSiguientePosicion = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        siguientePosicion = waypoints[0].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            siguientePosicion,
+            velocidad * Time.deltaTime);
+        
+        if (Vector3.Distance(transform.position,
+            siguientePosicion) < distanciaCambio)
+        {
+            numeroSiguientePosicion++;
+            if (numeroSiguientePosicion >= waypoints.Length)
+                numeroSiguientePosicion = 0;
+            siguientePosicion = 
+                waypoints[numeroSiguientePosicion].position;
+
+        }
+    }
+}
