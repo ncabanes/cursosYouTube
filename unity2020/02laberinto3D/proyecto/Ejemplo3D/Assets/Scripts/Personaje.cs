fe06eca87860715bea343f91b49088f721d10bd8
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-07 08:35:08 +0200

    Unity 2020 - Estado en el video 027 (Movimiento, versión 1)

---
+++ unity2020/02laberinto3D/proyecto/Ejemplo3D/Assets/Scripts/Personaje.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    float velocidad = 200.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        float avance = horizontal * velocidad * Time.deltaTime;
+        float lado = vertical * velocidad * Time.deltaTime;
+
+        GetComponent<Rigidbody>().velocity =
+            new Vector3(avance, 0, lado);
+
+    }
+}
