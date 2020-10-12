ee27431dd0cbec82c412f810124e9d57a9ce83bb
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-12 13:22:07 +0200

    Unity 2020 - Estado en el video 033 (Plataformas - Nivel 1)

---
+++ unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float velocidad = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(
+            horizontal * velocidad * Time.deltaTime,
+            0, 0);
+    }
+}
