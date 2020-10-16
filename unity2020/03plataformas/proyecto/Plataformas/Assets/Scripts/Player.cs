115bbf5e4c5dfa9c9cce3af8575f409a1a215ff0
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-16 13:10:42 +0200

    Unity 2020 - Estado en el video 034 (Plataformas - Saltos, cámara con personaje)

--- unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/Player.cs
+++ unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float velocidad = 20f;
+    float velocidad = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +19,12 @@ public class Player : MonoBehaviour
         transform.Translate(
             horizontal * velocidad * Time.deltaTime,
             0, 0);
+
+        float salto = Input.GetAxis("Jump");
+        if (salto > 0)
+        {
+            Vector3 fuerzaSalto = new Vector3(0, 10, 0);
+            GetComponent<Rigidbody2D>().AddForce(fuerzaSalto);
+        }
     }
 }
