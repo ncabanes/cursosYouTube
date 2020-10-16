8ed2ad797131990b49a8ea3233a749fcba7c5b43
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-16 13:13:48 +0200

    Unity 2020 - Estado en el video 038 (Items y GameController)

---
+++ unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/Diamante.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamante : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FindObjectOfType<GameController>().SendMessage("AnotarItemRecogido");
+        Destroy(gameObject);
+    }
+}
---
+++ unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private int puntos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        puntos = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AnotarItemRecogido()
+    {
+        puntos += 10;
+        Debug.Log("Puntos: " + puntos);
+    }
+}
