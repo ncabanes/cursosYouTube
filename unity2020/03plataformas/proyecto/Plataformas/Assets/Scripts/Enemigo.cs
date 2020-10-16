7828dd0da6db89ced09f369706c8fdca95299716
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-16 13:15:29 +0200

    Unity 2020 - Estado en el video 040 (Enemigos y vidas)

---
+++ unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/Enemigo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    [SerializeField] List<Transform> wayPoints;
+    float velocidad = 2;
+    float distanciaCambio = 0.2f;
+    byte siguientePosicion = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            wayPoints[siguientePosicion].transform.position,
+            velocidad * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position,
+            wayPoints[siguientePosicion].transform.position) < distanciaCambio)
+        {
+            siguientePosicion++;
+            if (siguientePosicion >= wayPoints.Count)
+                siguientePosicion = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        FindObjectOfType<GameController>().SendMessage("PerderVida");
+    }
+}
--- unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/GameController.cs
+++ unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/GameController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     private int puntos;
+    private int vidas;
 
     // Start is called before the first frame update
     void Start()
     {
         puntos = 0;
+        vidas = 3;
     }
 
     // Update is called once per frame
@@ -22,5 +24,18 @@ public class GameController : MonoBehaviour
     {
         puntos += 10;
         Debug.Log("Puntos: " + puntos);
+    }
+
+    public void PerderVida()
+    {
+        vidas--;
+        FindObjectOfType<Player>().SendMessage("Recolocar");
+        Debug.Log("Vidas: " + vidas);
+        if (vidas <= 0)
+        {
+            // TO DO: Ir al menú principal
+            Debug.Log("Partida terminada");
+            Application.Quit();
+        }
     }
 }
