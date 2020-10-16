21589967f52b843a87f3b460540cd877c7219766
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-16 13:12:51 +0200

    Unity 2020 - Estado en el video 037 (Raycasting)

--- unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/Player.cs
+++ unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/Player.cs
@@ -6,12 +6,14 @@ public class Player : MonoBehaviour
 {
     float xInicial, yInicial;
     float velocidad = 5f;
+    float alturaPersonaje;
 
     // Start is called before the first frame update
     void Start()
     {
         xInicial = transform.position.x;
         yInicial = transform.position.y;
+        alturaPersonaje = GetComponent<Collider2D>().bounds.size.y;
     }
 
     // Update is called once per frame
@@ -25,8 +27,19 @@ public class Player : MonoBehaviour
         float salto = Input.GetAxis("Jump");
         if (salto > 0)
         {
-            Vector3 fuerzaSalto = new Vector3(0, 10 , 0);
-            GetComponent<Rigidbody2D>().AddForce(fuerzaSalto);
+            RaycastHit2D impacto = Physics2D.Raycast(
+                transform.position,
+                new Vector2(0, -1));
+            if (impacto.collider != null)
+            {
+                float distanciaAlSuelo = impacto.distance;
+                bool tocandoElSuelo = distanciaAlSuelo < alturaPersonaje;
+                if (tocandoElSuelo)
+                {
+                    Vector3 fuerzaSalto = new Vector3(0, 10, 0);
+                    GetComponent<Rigidbody2D>().AddForce(fuerzaSalto);
+                }
+            }
         }
     }
 
