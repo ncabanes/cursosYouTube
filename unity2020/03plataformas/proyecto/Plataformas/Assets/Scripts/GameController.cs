f43be7fd3d720480a0ce42296ac7548cfe531679
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-16 13:18:09 +0200

    Unity 2020 - Estado en el video 042 (Dos niveles)

--- unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/GameController.cs
+++ unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/GameController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    private int itemsRestantes;
     private int puntos;
     private int vidas;
 
@@ -12,18 +14,25 @@ public class GameController : MonoBehaviour
     {
         puntos = 0;
         vidas = 3;
+        itemsRestantes = FindObjectsOfType<Diamante>().Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.N))
+            AvanzarNivel();
     }
 
     public void AnotarItemRecogido()
     {
         puntos += 10;
         Debug.Log("Puntos: " + puntos);
+
+        itemsRestantes--;
+        Debug.Log("Items restantes: " + itemsRestantes);
+        if (itemsRestantes <= 0)
+            AvanzarNivel();
     }
 
     public void PerderVida()
@@ -37,5 +46,10 @@ public class GameController : MonoBehaviour
             Debug.Log("Partida terminada");
             Application.Quit();
         }
+    }
+
+    public void AvanzarNivel()
+    {
+        SceneManager.LoadScene("Nivel2");
     }
 }
