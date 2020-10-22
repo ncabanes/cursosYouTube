f9ff81892908b0419556dbafc977340c80cb68e0
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-22 22:44:41 +0200

    Unity 2020 - Estado en el video 048 (Sonido en objetos que se destruyen)

--- unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/Diamante.cs
+++ unity2020/03plataformas/proyecto/Plataformas/Assets/Scripts/Diamante.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Diamante : MonoBehaviour
 {
+    [SerializeField] AudioClip sonido;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,7 @@ public class Diamante : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         FindObjectOfType<GameController>().SendMessage("AnotarItemRecogido");
+        AudioSource.PlayClipAtPoint(sonido, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
