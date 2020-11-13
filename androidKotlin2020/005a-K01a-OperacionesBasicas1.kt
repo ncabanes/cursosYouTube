b8823644d2c4e79ed7650363d0053afc4202c5de
Nacho Cabanes <nacho@nachocabanes.com>
2020-11-14 00:12:26 +0100

    Android+Kotlin 2020 - Ej.005 - Operaciones básicas (2 versiones)

---
+++ androidKotlin2020/005a-K01a-OperacionesBasicas1.kt
@@ -0,0 +1,14 @@
+// Repaso de operaciones básicas en Kotlin
+
+fun main() {
+    var n1 : Int = 23
+    var n2 : Int = 12
+    
+    println("n1 = " + n1)
+    println("n2 = " + n2)
+    println("n1+n2 = " + (n1+n2))
+    println("n1-n2 = " + (n1-n2))
+    println("n1*n2 = " + (n1*n2))
+    println("n1/n2 = " + (n1/n2))
+    println("n1%n2 = " + (n1%n2))
+}
