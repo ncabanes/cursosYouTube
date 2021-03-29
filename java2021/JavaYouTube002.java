565f77e03d9b82c4db0920fb5867a5a2934b644a
Nacho Cabanes <nacho@nachocabanes.com>
2021-03-29 14:00:39 +0200

    Ejercicios de Java 002: Operaciones aritméticas básicas

---
+++ java2021/JavaYouTube002.java
@@ -0,0 +1,10 @@
+public class JavaYouTube002 {
+     public static void main(String[] args){
+        System.out.println(1234+567);
+        System.out.println(1234-567);
+        System.out.println(1234*567);
+        System.out.println(1234/567);
+        System.out.println(1234%567);
+        System.out.println(1234.0/567);
+     }
+}
