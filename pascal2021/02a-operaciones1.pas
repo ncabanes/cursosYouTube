f787f9e785b405abbe81c83da7e25f5f3f5e8b95
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-11-21 22:48:06 +0100

    Ejercicio de la entrega 02 (Operaciones aritméticas)

---
+++ pascal2021/02a-operaciones1.pas
@@ -0,0 +1,6 @@
+program Operacion1;
+begin
+  write ('5 + 7 = ');
+  write (5 + 7);
+end.
+
---
+++ pascal2021/02b-operaciones2.pas
@@ -0,0 +1,10 @@
+program Operaciones2;
+begin
+  writeln(15 + 2);
+  writeln(15 - 2);
+  writeln(15 * 2);
+  writeln(15 / 2);
+  writeln(15 div 2);
+  writeln(15 mod 2);
+end.
+
