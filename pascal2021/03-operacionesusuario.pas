a2e716d43dd48c61bdc22f92b7fb0266e9ffd17e
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-11-23 11:53:13 +0100

    Pascal 2021 03 : Datos por usuario (var, readln)

---
+++ pascal2021/03-operacionesusuario.pas
@@ -0,0 +1,13 @@
+program OperacionesUsuario;
+var
+  n1 : integer;
+  n2 : integer;
+begin
+  writeln('Dime el primer numero');
+  readln(n1);
+  writeln('Dime el segundo numero');
+  readln(n2);
+  writeln('Su suma es:');
+  writeln( n1 + n2 );
+end.
+
