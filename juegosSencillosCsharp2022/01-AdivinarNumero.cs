c1aef6c7972b2e21e17ca1b5dfa7e16ebb663f55
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-01-31 18:54:21 +0100

    Juegos C# 01: Adivinar un número

---
+++ juegosSencillosCsharp2022/01-AdivinarNumero.cs
@@ -0,0 +1,60 @@
+﻿// Juego de adivinar un número (del 1 al 1000, en 10 intentos)
+
+using System;
+
+class AdivinarNumero
+{
+    static void Main(string[] args)
+    {
+        int aleatorio;
+        int introducido;
+        int restantes;
+
+        Console.WriteLine("Adivinar números");
+
+        // Generar aleatorio
+        Random generador = new Random();
+        aleatorio = generador.Next(1, 1001);
+
+        // restantes = 10
+        restantes = 10;
+
+        // Repetir
+        do
+        {
+            //   Pedir numero al usuario
+            Console.WriteLine("Intentos restantes: " + restantes);
+            Console.Write("Dime un número: ");
+            introducido = Convert.ToInt32(Console.ReadLine());
+
+            //   Si numero > aleatorio, Escribir "Te has pasado"
+            if (introducido > aleatorio)
+            {
+                Console.WriteLine("Te has pasado");
+            }
+
+            //   Si numero < aleatorio, Escribir "Te has quedado corto"
+            if (introducido < aleatorio)
+            {
+                Console.WriteLine("Te has quedado corto");
+            }
+
+            //   restantes = restantes - 1
+            restantes--;
+
+            // Hasta que numero = aleatorio o restantes = 0
+        }
+        while ((introducido != aleatorio) && (restantes > 0));
+
+        // Si numero = aleatorio, Escribir "Felicidades"
+        if (introducido == aleatorio)
+        {
+            Console.WriteLine("Acertaste!");
+        }
+        // Si no, Escribir "Era " + aleatorio
+        else 
+        {
+            Console.WriteLine("Era " + aleatorio);
+        }
+    }
+}
--- juegosSencillosCsharp2022/README.md
+++ juegosSencillosCsharp2022/README.md
@@ -4,9 +4,10 @@
 
 Lista de reproducción: https://www.youtube.com/playlist?list=PL0bfr51v6JJE3qcKPjjcjKlhAQ4wejfeV
 
-Fuentes disponibles en: https://github.com/ncabanes/cursosYouTube/juegosSencillosCsharp2022
+Fuentes disponibles en: https://github.com/ncabanes/cursosYouTube/tree/master/juegosSencillosCsharp2022
 
 Contenido disponible hasta ahora:
 
  - 00 - Presentación (06:59) https://youtu.be/NKXC5VqlZ1o
+ - 01 - Adivinar un número (16:06) https://youtu.be/rwrSE7XkHgc
 
